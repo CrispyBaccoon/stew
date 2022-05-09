95df89edb156537a92199e23bb61c64f8a03be8b
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-05-09 19:10:11 +0200

    Changed comments to multilinecomments

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const [command, ...args] = process.argv.slice(2);
 
 switch (command) {
   case "setup": {
-    // Takes [ name ]. Adds symlinks to each location for the folder/file with *name* in stewpath.
+    /* Takes [ name ]. Adds symlinks to each location for the folder/file with *name* in stewpath. */
     if (args[0] != undefined) {
       app.setup(args[0]);
     } else {
@@ -17,7 +17,7 @@ switch (command) {
     break;
   }
   case "add": {
-    // Takes [ path, name ]. Moves folder/file at *path* to stewpath with *name*. Also adds this to your config.
+    /* Takes [ path, name ]. Moves folder/file at *path* to stewpath with *name*. Also adds this to your config. */
     if (args[0] != undefined && args[1] != undefined) {
       app.add(args[0], args[1]);
     } else {
@@ -27,7 +27,7 @@ switch (command) {
     break;
   }
   case "link": {
-    // Takes [ path, name ]. **ONLY IF *path* DOES NOT EXIST**. Will add *path* to *name* in config and link.
+    /* Takes [ path, name ]. **ONLY IF *path* DOES NOT EXIST**. Will add *path* to *name* in config and link. */
     if (args[0] != undefined && args[1] != undefined) {
       app.link(args[0], args[1]);
     } else {
@@ -41,4 +41,4 @@ switch (command) {
   }
 }
 
-// vim: ft=javascript
+/* vim: ft=javascript */
